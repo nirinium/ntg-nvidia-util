43f75fddc9b0f027c0195f6f8f081d852fcee246
Nirinium <nirinium@gmail.com>
2018-12-21 05:53:57 -0500

    Additions.. still a long way to go

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,51 +30,76 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkForUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ntgLog1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button5 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.optionsToolStripMenuItem,
+            this.aboutToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(423, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(422, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkForUpdatesToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // optionsToolStripMenuItem
+            // checkForUpdatesToolStripMenuItem
             // 
-            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.optionsToolStripMenuItem.Text = "Options";
+            this.checkForUpdatesToolStripMenuItem.Name = "checkForUpdatesToolStripMenuItem";
+            this.checkForUpdatesToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.checkForUpdatesToolStripMenuItem.Text = "Check For Updates";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 495);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(423, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(422, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -84,21 +109,135 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
             this.toolStripStatusLabel1.Text = "Status:";
             // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 466);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Export Log...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ntgLog1
+            // 
+            this.ntgLog1.Location = new System.Drawing.Point(12, 255);
+            this.ntgLog1.Name = "ntgLog1";
+            this.ntgLog1.Size = new System.Drawing.Size(398, 205);
+            this.ntgLog1.TabIndex = 4;
+            this.ntgLog1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(222, 466);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Copy To Clipboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(124, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Scan Again";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 180);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 45);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(156, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Automatically export logfile?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 466);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Version History";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::NTG_NVIDIA_UTIL.Properties.Resources.nvidia;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(398, 147);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 231);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(398, 18);
+            this.progressBar.TabIndex = 11;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(223, 192);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(187, 25);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Download Utilities";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 450);
+            this.ClientSize = new System.Drawing.Size(422, 517);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ntgLog1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "NTG - NVIDIA UTILITY";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "nVutil | konsol3.com";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,10 +247,23 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox ntgLog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolStripMenuItem checkForUpdatesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Management;
+using System.IO;
+using HtmlAgilityPack;
+using System.Net;
 
 namespace NTG_NVIDIA_UTIL
 {
@@ -20,23 +23,109 @@ namespace NTG_NVIDIA_UTIL
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Log start time
+            ntgLog1.AppendText(DateTime.Now.ToString("yyyyMMdd_hhmmss") + "\r\n");
+            //Poll hardware
+            Hardware_Poll();
+        }
+
+        private void Hardware_Poll()
+        {
+            ntgLog1.ReadOnly = true;
+
             ManagementObjectSearcher myVideoObject = new ManagementObjectSearcher("select * from Win32_VideoController");
 
             foreach (ManagementObject obj in myVideoObject.Get())
             {
-                Console.WriteLine("Name  -  " + obj["Name"]);
-                Console.WriteLine("Status  -  " + obj["Status"]);
-                Console.WriteLine("Caption  -  " + obj["Caption"]);
-                Console.WriteLine("DeviceID  -  " + obj["DeviceID"]);
-                Console.WriteLine("AdapterRAM  -  " + obj["AdapterRAM"]);
-                Console.WriteLine("AdapterDACType  -  " + obj["AdapterDACType"]);
-                Console.WriteLine("Monochrome  -  " + obj["Monochrome"]);
-                Console.WriteLine("InstalledDisplayDrivers  -  " + obj["InstalledDisplayDrivers"]);
-                Console.WriteLine("DriverVersion  -  " + obj["DriverVersion"]);
-                Console.WriteLine("VideoProcessor  -  " + obj["VideoProcessor"]);
-                Console.WriteLine("VideoArchitecture  -  " + obj["VideoArchitecture"]);
-                Console.WriteLine("VideoMemoryType  -  " + obj["VideoMemoryType"]);
+                //Shitty way of doing this im aware.
+                ntgLog1.AppendText("Graphics Card  -  " + obj["Name"] + " Status  -  " + obj["Status"] + (Environment.NewLine));
+                //ntgLog1.AppendText("Name  -  " + obj["Name"] + (Environment.NewLine));
+                //ntgLog1.AppendText("Status  -  " + obj["Status"] + (Environment.NewLine));
+                //ntgLog1.AppendText("Caption  -  " + obj["Caption"] + (Environment.NewLine));
+                ntgLog1.AppendText("DeviceID  -  " + obj["DeviceID"] + (Environment.NewLine));
+                ntgLog1.AppendText("AdapterRAM  -  " + obj["AdapterRAM"] + (Environment.NewLine));
+                ntgLog1.AppendText("AdapterDACType  -  " + obj["AdapterDACType"] + (Environment.NewLine));
+                ntgLog1.AppendText("Monochrome  -  " + obj["Monochrome"] + (Environment.NewLine));
+                ntgLog1.AppendText("DriverVersion  -  " + obj["DriverVersion"] + (Environment.NewLine));
+                ntgLog1.AppendText("VideoProcessor  -  " + obj["VideoProcessor"] + (Environment.NewLine));
+                ntgLog1.AppendText("VideoArchitecture  -  " + obj["VideoArchitecture"] + (Environment.NewLine));
+                ntgLog1.AppendText("VideoMemoryType  -  " + obj["VideoMemoryType"] + "\n\n" );
+                ntgLog1.AppendText("InstalledDisplayDrivers  -  " + obj["InstalledDisplayDrivers"] + "\n");
+                //*[@id="driverList"]/td[2]/b/a
+            }
+
+
+            toolStripStatusLabel2.Text = "Idle";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ntgLog1.SaveFile("logfile" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".txt", RichTextBoxStreamType.PlainText);
+            toolStripStatusLabel2.Text = "Saved log!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ntgLog1.SelectAll();
+            ntgLog1.Copy();
+            toolStripStatusLabel2.Text = "Copied Log to Clipboard!";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ntgLog1.Clear();
+            Hardware_Poll();
+            toolStripStatusLabel2.Text = "Manually scanned video properties";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Clear Log
+            ntgLog1.Clear();
+
+            string nvURL = "https://www.nvidia.com/Download/processFind.aspx?psid=101&pfid=816&osid=57&lid=1&whql=1&lang=en-us&ctk=0";
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(nvURL);
+
+            var nvDrivers = doc.DocumentNode.SelectNodes("//*[@id='driverList']/td[3]").ToList();
+
+            ntgLog1.AppendText("Driver Version History: \n");
+
+            foreach (var driver in nvDrivers)
+            {
+                ntgLog1.AppendText(driver.InnerText + "\n");
+                //*[@id='driverList']/td[3] WORKS
+                //*[@id='driverList']/td[2]/b/a
             }
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.wagnardsoft.com/DDU/download/DDU%20v18.0.0.6.exe";
+                using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileAsync(new System.Uri(url), "ddu.exe");                                
+            }
+
+            
+        }
+        // Event to track the progress
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+
+            //if (File.Exists("ddu.exe"))
+            if (progressBar.Value >= 100) //has to be greater than or equal to cant just be equal.. maybe ==? 
+            {
+                toolStripStatusLabel2.Text = "Download complete!";
+            }
+        }
+    
     }
 }
